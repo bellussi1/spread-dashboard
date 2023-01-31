9997d56dc578eb4e8e13d63d591c6d2a82f143c5
Bellussi <113950272+bellussi1@users.noreply.github.com>
2023-01-31 16:19:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ com base nas opções de filtro selecionadas, usando o callback.
 # Carregando o arquivo CSV para um dataframe
 df = pd.read_csv("data/spread.csv")
 
+bank_colors = {  # Dicionario de cores para os principais Bancos
+    "BB": "#F9DD16",
+    "ITAU": "#140083",
+    "BRADESCO": "#CC092F",
+    "SANTANDER": "#8B0000",
+}
 # Criando o dicionário de substituições
 quarter_replace = {1: "03", 2: "06", 3: "09", 4: "12"}
 
@@ -28,7 +34,7 @@ df["ANO-TRIMESTRE"] = pd.to_datetime(df["ANO-TRIMESTRE"], format="%Y-%m")
 
 # Criando a coluna total resultado (despesas + lucro)
 df["TOTAL_OP"] = df.apply(lambda x: int(x["RESULT_OP"]) + x["DESPESA_OP"], axis=1)
-
+df["TOTAL_N"] = df.apply(lambda x: int(x["NUMERO_OP"]) + x["NUMERO_INTERBANK"], axis=1)
 years = df["ANO"].unique()  # Anos unicos para o filtro
 banks = df["NOME_BANCO"].unique()  # Bancos unicos para o filtro
 quarters = df["TRIMESTRE"].unique()  # Trimestres unicos para o filtro
@@ -39,12 +45,10 @@ app = Dash(__name__)
 # Criando o Layout para o Dashboard
 app.layout = html.Div(
     [
-        html.H1("Dashboard Test", className="title"),  # Titulo
-        html.Div(  # Div para o Texto do Filtro
-            html.H4("Filtro", className="filter"), className="filter-container"
-        ),  # Texto para Filtro
         html.Div(  # Div dos Dropdowns
             [
+                html.H1("Dashboard Test", className="title"), # Titulo
+                html.Label("Ano", className="dropdown-labels"),
                 dcc.Dropdown(  # Filtro do Ano, retornando os 4 trimestres como padrão
                     id="year-dropdown",
                     options=[  # Valores unicos a filtrar
@@ -56,6 +60,7 @@ app.layout = html.Div(
                     style=dict(color="black"),
                     optionHeight=50,  # Altura das opções (Estetica)
                 ),
+                html.Label("Trimestre", className="dropdown-labels"),
                 dcc.Dropdown(  # Filtro do Trimestre, retornando os 4 trimestres como padrão
                     id="quarter-dropdown",
                     options=[  # Valores unicos a filtrar
@@ -66,6 +71,7 @@ app.layout = html.Div(
                     className="dropdown",
                     optionHeight=50,  # Altura das opções (Estetica)
                 ),
+                html.Label("Instituição Bancária", className="dropdown-labels"),
                 dcc.Dropdown(  # Filtro do Banco, retornando os 4 principais como padrão
                     id="bank-dropdown",
                     options=[  # Valores unicos a filtrar
@@ -84,31 +90,28 @@ app.layout = html.Div(
             ],
             className="dropdown-container",
         ),
-        html.Div(  # Div do Grafico
-            [dcc.Graph(id="operations-result-lineplot", className="graph")],
+        html.Div(
+            [  # Div do Grafico
+                html.Div(
+                    [dcc.Graph(id="operations-result-lineplot", className="lineplot")],
+                    className="lineplot-container",
+                ),
+                html.Div(
+                    [dcc.Graph(id="operations-result-pieplot", className="pieplot")],
+                    className="pieplot-container",
+                ),
+            ],
             className="graph-container",
         ),
     ],
     className="main-container",
 )
 
-"""
-A função update_graph é executada quando o valor de uma das três seleções dropdown é alterado. 
-Ele filtra o dataframe original com base nas opções de filtro selecionadas e, em seguida, agrupa os dados por banco,
-ano e trimestre e calcula a soma dos resultados das operações. 
-"""
 
-@app.callback(
-    Output("operations-result-lineplot", "figure"),  # Output da figure
-    [  # Parametros de inputs (Ano, Trimestre e Banco)
-        Input("year-dropdown", "value"),
-        Input("quarter-dropdown", "value"),
-        Input("bank-dropdown", "value"),
-    ],
-)
-def update_lineplot(selected_year, selected_quarter, selected_bank):
-    # Função em que pega os Ano, Trimestre e Bancos selecionados pelo Dropdown
-    # E retorna o LinePlot com os valores filtrados
+def filter_data(selected_year, selected_quarter, selected_bank, df=df):
+    """
+    Função de filtrar quais valores selecionados do dataframe
+    """
     if not isinstance(selected_year, list):
         selected_year = [selected_year]
     if not isinstance(selected_quarter, list):
@@ -116,33 +119,28 @@ def update_lineplot(selected_year, selected_quarter, selected_bank):
     if not isinstance(selected_bank, list):
         selected_bank = [selected_bank]
 
-    filtered_df = df[  # Filtrando o dataframe com os parametros escolhidos
+    filtered_df = df[
         (df["ANO"].isin(selected_year))
         & (df["TRIMESTRE"].isin(selected_quarter))
         & (df["NOME_BANCO"].isin(selected_bank))
     ]
 
-    if (
-        "ANO-TRIMESTRE" not in filtered_df.columns
-    ):  # Certificando se o periodo selecionado há no dataframe
-        filtered_df[
-            "ANO-TRIMESTRE"
-        ] = filtered_df.apply(  # Formatando a junção da coluna Ano e trimestre
+    if "ANO-TRIMESTRE" not in filtered_df.columns:
+        filtered_df["ANO-TRIMESTRE"] = filtered_df.apply(
             lambda x: str(x["ANO"]) + "-" + x["TRIMESTRE"], axis=1
         )
 
-        filtered_df[
-            "ANO-TRIMESTRE"
-        ] = pd.to_datetime(  # Convertendo os valores para tipo Date
+        filtered_df["ANO-TRIMESTRE"] = pd.to_datetime(
             filtered_df["ANO-TRIMESTRE"], format="%Y-%m"
         )
 
-    bank_colors = {  # Dicionario de cores para os principais Bancos
-        "BB": "#F9DD16",
-        "ITAU": "#002347",
-        "BRADESCO": "#420C09",
-        "SANTANDER": "#D0312D",
-    }
+    return filtered_df
+
+
+def update_lineplot(filtered_df):
+    """
+    Função que cria um lineplot com os filtros selecionados
+    """
 
     fig = px.line(  # Criando a figura de Visualização
         filtered_df,  # Dataframe filtrado pelo dropdown
@@ -170,12 +168,12 @@ def update_lineplot(selected_year, selected_quarter, selected_bank):
                 family="Arial",  # Familia da fonte
             ),
             x=0.46,  # Posição no eixo X
-            y=0.9,  # Posição no eixo y
+            y=.95,  # Posição no eixo y
         ),
         xaxis=dict(  # Valor em X
             title=dict(
                 font=dict(
-                    size=18,  # Tamanho da fonte
+                    size=14,  # Tamanho da fonte
                     color="#0D214F",  # Cor da fonte
                     family="Arial",  # Familia da fonte
                 ),
@@ -184,7 +182,7 @@ def update_lineplot(selected_year, selected_quarter, selected_bank):
         yaxis=dict(
             title=dict(
                 font=dict(
-                    size=18,  # Tamanho da fonte
+                    size=14,  # Tamanho da fonte
                     color="#0D214F",  # Cor da fonte
                     family="Arial",  # Familia da fonte
                 ),
@@ -192,23 +190,106 @@ def update_lineplot(selected_year, selected_quarter, selected_bank):
         ),
         legend=dict(
             font=dict(  # Fonte da Legenda
-                size=14,  # Tamanho da fonte
+                size=11,  # Tamanho da fonte
                 color="#0D214F",  # Cor da fonte
                 family="Arial",  # Familia da fonte
             ),
             title=dict(  # Titulo da Legenda
                 text="INSTITUIÇÕES BANCÁRIAS",  # String
                 font=dict(  # Fonte do Titulo da Leganda
-                    size=18,  # Tamanho da fonte
+                    size=13,  # Tamanho da fonte
                     color="#0D214F",  # Cor da fonte
                     family="Arial",  # Familia da fonte
                 ),
             ),
         ),
+        margin=dict(l=1, r=1, t=50, b=5),
         transition_duration=1000,  # Tempo de transição na atulização do gráfico
     )
+    return fig
 
-    return fig  # Retorna a figura
+
+def update_pieplot(filtered_df):
+    fig = px.pie(
+        filtered_df,
+        values="TOTAL_N",
+        names="NOME_BANCO",
+        title="Participação do Banco em (%) no periodo",
+        color="NOME_BANCO",
+        color_discrete_map=bank_colors,
+    )
+
+    fig.update_traces(textposition="inside", textinfo="percent+label")
+    fig.update_layout(  # Estilizando a Visualização
+        paper_bgcolor="white",  # Cor em volta do Plot
+        # plot_bgcolor="#0D214F", # Cor no fundo do Plot
+        title=dict(  # Titulo
+            font=dict(
+                size=20,  # Tamanho da fonte
+                color="#0D214F",  # Cor da fonte
+                family="Arial",  # Familia da fonte
+            ),
+            x=0.46,  # Posição no eixo X
+            y=0.95,  # Posição no eixo y
+        ),
+        xaxis=dict(  # Valor em X
+            title=dict(
+                font=dict(
+                    size=14,  # Tamanho da fonte
+                    color="#0D214F",  # Cor da fonte
+                    family="Arial",  # Familia da fonte
+                ),
+            )
+        ),
+        yaxis=dict(
+            title=dict(
+                font=dict(
+                    size=14,  # Tamanho da fonte
+                    color="#0D214F",  # Cor da fonte
+                    family="Arial",  # Familia da fonte
+                ),
+            )
+        ),
+        legend=dict(
+            font=dict(  # Fonte da Legenda
+                size=11,  # Tamanho da fonte
+                color="#0D214F",  # Cor da fonte
+                family="Arial",  # Familia da fonte
+            ),
+            title=dict(  # Titulo da Legenda
+                text="INSTITUIÇÕES BANCÁRIAS",  # String
+                font=dict(  # Fonte do Titulo da Leganda
+                    size=13,  # Tamanho da fonte
+                    color="#0D214F",  # Cor da fonte
+                    family="Arial",  # Familia da fonte
+                ),
+            ),
+        ),
+        transition = dict(duration = 1500),  # Tempo de transição na atulização do gráfico
+    )
+    return fig
+
+
+@app.callback(
+    Output("operations-result-lineplot", "figure"),
+    Output("operations-result-pieplot", "figure"),
+    # Output("operations-interbank-primary-barplot", "figure"),
+    # Output da figure
+    [  # Parametros de inputs (Ano, Trimestre e Banco)
+        Input("year-dropdown", "value"),
+        Input("quarter-dropdown", "value"),
+        Input("bank-dropdown", "value"),
+    ],
+)
+def update_plots(selected_year, selected_quarter, selected_bank):
+    """
+    Atualiza os valores para os gráficos
+    """
+    filtered_df = filter_data(selected_year, selected_quarter, selected_bank)
+    line_plot = update_lineplot(filtered_df)
+    pie_plot = update_pieplot(filtered_df)
+    return line_plot, pie_plot
+    # Retorna a figura
 
 
 if __name__ == "__main__":  # Iniciando o o Dashboard
